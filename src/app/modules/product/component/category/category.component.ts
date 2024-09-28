ba456004f02ee25abebced7f67e13112f3d8af4d
FranciscoMendiola <franciscomendiola9@ciencias.unam.mx>
2024-09-27 22:58:51 -0600

    Update puntos 3 y 4

--- src/app/modules/product/component/category/category.component.ts
+++ src/app/modules/product/component/category/category.component.ts
@@ -1,22 +1,78 @@
 import { Component } from '@angular/core';
-import { CategoryService } from '../../_service/category.service';
 import { Category } from '../../_model/category';
+import { CategoryService } from '../../_service/category.service';
+import { SharedModule } from '../../../../shared/shared-module';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { SwalMessages } from '../../../../shared/swal-messages';
+
+
+declare var $: any; // jquery
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
-  categories: Category[]=[];
-  constructor(private categoryService:CategoryService){}
 
-  getCategories():void{
+
+export class CategoryComponent {
+
+  // Lista de categorías
+  categories: Category[] = [];
+
+  submitted = false; // form submitted
+  swal: SwalMessages = new SwalMessages(); // swal messages
+  form: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+
+    // Inicializacion el formulario en ngOnInit
+    this.form = this.formBuilder.group({
+      category: ["", [Validators.required]],
+      tag: ["", [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+
+    // validación del formulario 
+    this.submitted = true;
+    if (this.form.invalid) { return; }
+    this.submitted = false;
+
+    // creamos nueva categoria y la agrega a la lista
+    let id = this.categories.length + 1;
+    let category = new Category(id, this.form.controls['category'].value!, this.form.controls['tag'].value!, 1);
+    this.categories.push(category);
+
+    // reseteamos el formulario
+    this.hideModalForm();
+    this.form.reset();
+    this.swal.successMessage("¡La categoría ha sido registrada!");
+  }
+
+
+  getCategories(): void {
     this.categories = this.categoryService.getCategories();
   }
-  ngOnInit():void{
-    this.getCategories();
+
+
+  showModalForm() {
+    this.form.reset();
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
+
+  hideModalForm() {
+    $("#modalForm").modal("hide");
   }
 }
